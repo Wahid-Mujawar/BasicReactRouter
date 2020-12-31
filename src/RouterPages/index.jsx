28e57292febea33fea3ec041a076f73839dd2de2
Wahid-Mujawar <abdulwahid0218@gmail.com>
2020-12-30 16:04:50 -0800

    test

--- src/RouterPages/index.jsx
+++ src/RouterPages/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MainPage = () => {
    return (
       <div>
          <h3>Basic React Router (Multi-Pages-Apps)</h3>
          <h2>Main Page</h2>
+         <Link to="/users">Show List Of Users</Link>
       </div>
    )
 }
